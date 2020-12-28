2b5db434d6e8d74fdb6da03f5d4aa644fd21139d
Erdem Camlioglu <secamlioglu@gmail.com>
2020-12-28 09:22:42 +0300

    led fix

--- led.py
+++ led.py
@@ -13,6 +13,7 @@ try:
         GPIO.setmode(GPIO.BOARD)
         GPIO.setwarnings(False)
         GPIO.setup(18,GPIO.OUT)
+        GPIO.setup(15,GPIO.OUT)
         GPIO.setup(16,GPIO.OUT)
         p=0
         # for i in range(0,5):
